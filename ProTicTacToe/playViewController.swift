5a17e70ba81850061237dd2a585fceb229a8dd15
Alex Jenke <alex@jenke.de>
2016-09-22 16:27:16 +0200

    [BUGFIX] bugfix appflow

--- ProTicTacToe/playViewController.swift
+++ ProTicTacToe/playViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 class playViewController: UIViewController {
+
     @IBAction func button(sender: UIButton) {
             performSegueWithIdentifier("pttt3d", sender: self)
     }
