5a2500a7f4ac8a279cd81256419ff0d1f00ca906
Leonard Follner <Leonard.Follner@gmail.com>
2016-09-22 14:28:20 +0200

    [FEATURE] started working on menu

---
+++ ProTicTacToe/MenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuVieController.swift
+//  ProTicTacToe
+//
+//  Created by Leonard Follner on 22.09.16.
+//  Copyright © 2016 Alex Jenke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MenuViewController: UIViewController {
+    
+    @IBAction func scanButton (sender: UIButton!) {
+        
+        performSegueWithIdentifier("3d", sender: self)
+        
+    }
+}
